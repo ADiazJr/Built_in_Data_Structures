f00c11e960309e6d4d6c6b4a8caa4e4e529e61e2
unknown <arturodiazmay@gmail.com>
2022-09-07 14:31:11 -0500

    fix. changed into functions and called to main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import implementation
 
 implementation.pi_month()
-implementation.print_fruits_or_veggies
-implementation.print_user_profile
-implementation.print_name_relation
+implementation.print_fruits_or_veggies()
+implementation.print_user_profile()
+implementation.print_name_relation()
