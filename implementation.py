b30a0e869ad423ef193e55f04031ece02bf5be40
unknown <arturodiazmay@gmail.com>
2022-09-07 14:29:51 -0500

    fix. changed into functions and called to main.py

--- implementation.py
+++ implementation.py
@@ -1,45 +1,49 @@
 # Task 1a)
-months_of_year = ('january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december')
-print(months_of_year[2])
+def pi_month():
+    months_of_year = ('january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december')
+    print(months_of_year[2])
 #1b)
-fruits_or_veggies = ['apple', 'banana', 'tomato', 'lettuce', 'strawberry']
-fruits_or_veggies.extend(['mango', 'grape', 'eggplant', 'pepper'])
-for item in fruits_or_veggies:
-    print(item)
+def print_fruits_or_veggies():
+    fruits_or_veggies = ['apple', 'banana', 'tomato', 'lettuce', 'strawberry']
+    fruits_or_veggies.extend(['mango', 'grape', 'eggplant', 'pepper'])
+    for item in fruits_or_veggies:
+        print(item)
 
 #1c)
-user_profile = {
-    'first_name': 'peter',
-    'last_name': 'parker',
-    'email_address': 'spidey@gmail.com',
-    'phone_number': 2324513245
-}
+def print_user_profile():
+    user_profile = {
+        'first_name': 'peter',
+        'last_name': 'parker',
+        'email_address': 'spidey@gmail.com',
+        'phone_number': 2324513245
+    }
 
-print(f"""User's first name is: {user_profile['first_name']}
-User's last name: {user_profile['last_name']}
-User's Email: {user_profile['email_address']}
-User's Phone Number: {user_profile['phone_number']}
-""")
+    print(f"""User's first name is: {user_profile['first_name']}
+    User's last name: {user_profile['last_name']}
+    User's Email: {user_profile['email_address']}
+    User's Phone Number: {user_profile['phone_number']}
+    """)
 
 #Task 2
-immediate_family = [
-    {
-        'first_name': 'Arturo',
-        'last_name': 'Diaz',
-        'relation': 'father'
-    },
-    {
-        'first_name': 'Dimari',
-        'last_name': 'Diaz',
-        'relation': 'sister'
-    },
-    {
-        'first_name': 'Irene',
-        'last_name': 'Rodriguez',
-        'relation': 'mother'
-    }
-]
+def print_name_relation():
+    immediate_family = [
+        {
+            'first_name': 'Arturo',
+            'last_name': 'Diaz',
+            'relation': 'father'
+        },
+        {
+            'first_name': 'Dimari',
+            'last_name': 'Diaz',
+            'relation': 'sister'
+        },
+        {
+            'first_name': 'Irene',
+            'last_name': 'Rodriguez',
+            'relation': 'mother'
+        }
+    ]
 
-for dictionary in immediate_family:
-    print(dictionary['first_name'])
-    print(dictionary['relation'])
+    for dictionary in immediate_family:
+        print(dictionary['first_name'])
+        print(dictionary['relation'])
